a5774b788a8d3cd3a035b48929b628817fcb0e2c
amdugar <amit@browserstack.com>
2013-08-07 21:17:53 +0530

    changes

--- osxautomation.h
+++ osxautomation.h
@@ -1,3 +1,4 @@
+#include <X.h>
 void processCommand(const char *cmd);
 
 void print_msg(const char *msg);
@@ -10,12 +11,15 @@ void mousePress(int btn, int clickType);
 void mouseRelease(int btn, int clickType);
 void mouseClick(int btn, int clickType);
 void mouseDrag(int btn, int posX, int posY);
+void mouseScrollX(int length);
+void mouseScrollY(int length);
 void typeString(char *str);
 void keyHit(int kc, CGEventFlags flags);
 void keyPress(int kc, CGEventFlags flags);
 void keyRelease(int kc, CGEventFlags flags);
 void toKey(CGKeyCode kc, CGEventFlags flags, bool upOrDown);
-
+void unicodeKeyPress(long key);
+long keysym2ucs(long keysym);
 // iGetKey crap
 typedef struct {
   short kchrID;
